d9d8b4f4e77cee60899c773ef89e647b8353ab52
Unknown <bachrathyd@gmail.com>
2019-02-05 16:50:26 +0100

    Type stable corners and function evaluation
    problem: non-vectorized function call

--- src/MDBM__types.jl
+++ src/MDBM__types.jl
@@ -22,79 +22,48 @@ struct MemF{RTf,RTc,AT} <:Function
     MemF(f::Function,c::Function,a::Vector{MDBMcontainer{RTf,RTc,AT}}) where {RTf,RTc,AT}=new{RTf,RTc,AT}(f,c,a,[Int64(0)])
 end
 
-
 #------------SH version - hidden 'Any'-------------
 (memfun::MemF{RTf,RTc,AT})(::Type{RTf},::Type{RTc},args...,) where {RTf,RTc,AT} =( memfun.f(args...,)::RTf, memfun.c(args...,)::RTc)
 
 function (memfun::MemF{RTf,RTc,AT})(args...,) where {RTf,RTc,AT}
+    #println("As a normal function call")
+    # println(args)
     location=searchsortedfirst(memfun.fvalarg,args,lt=(x,y)->isless(x.callargs,y));
+    # println(location)
+    # println(length(memfun.fvalarg))
     if length(memfun.fvalarg)<location
-        #println("ujraszamolas a végére")
+        # println("ujraszamolas a végére")
         x=memfun(RTf,RTc,args...,);
         push!(memfun.fvalarg,MDBMcontainer{RTf,RTc,AT}(x...,args))
         return x
     elseif  memfun.fvalarg[location].callargs!=args
-        #println("ujraszamolas közé illeszt")
+        # println("ujraszamolas közé illeszt")
+        # println(memfun.fvalarg[location].callargs)
+        # println(args)
         x=memfun(RTf,RTc,args...,);
         insert!(memfun.fvalarg, location, MDBMcontainer{RTf,RTc,AT}(x...,args))
         return x
     else
-        #println("mar megvolt")
+        # println("mar megvolt")
         memfun.memoryacc[1]+=1;
         return (memfun.fvalarg[location].funval,memfun.fvalarg[location].cval);
     end
 end
-#-------------------------
+function (memfun::MemF{RTf,RTc,AT})(args::Tuple) where {RTf,RTc,AT}
+    #println("Function call with a Tuple---------")
+    memfun(args...,)
+end
 
-# function indexin_sorted(a::Array{T,1}, b::Array{T,1})::Array{Int64,1} where T
-#         # b must contain all the lements of a
-#         # a and b must be sorted
-#     if isempty(a)
-#         return Array{Int64}(undef,0)
-#     elseif length(b) == 1 ##much faster!
-#         return Int64.(a .== b)#*Int64(1)
-#     else
-#         out = Array{Int64}(undef, size(a));#zeros(T, size(a))
-#         leng::Int64 = length(b);
-#
-#         q::Int64 = 1;
-#         q1::Int64 = 1;
-#         q2::Int64 = length(b);
-#         for k = 1:length(a)
-#
-#             if (q2 - q1) == 1
-#                 q = (b[q1] == a[k]) ? q1 : q2
-#             else
-#                 while (b[q] != a[k]) & (q2 > q1 + 1)
-#                     if b[q] > a[k]
-#                         q2 = q
-#                         q =  (q + q1+1) ÷ 2
-#                     else
-#                         q1 = q
-#                         q = (q + q2) ÷ 2
-#                     end
-#             #print([q1;q;q2])
-#             #print([b[q]])
-#             #println([a[k]])
-#                 end
-#             end
-#             out[k] = b[q] == a[k] ? q : 0
-#             q = max(out[k], q1)
-#             q1 = q
-#             q2 = length(b)
-#           #print("-----")
-#           #print(out[1:k])
-#           #println("----")
-#             if q1 > length(b)#all the element is larger than the last one
-#                 break
-#             end
-#         end
-#         return out
-#     end
-# end
-#
-#
-# # indexin_sorted([-5,0,1,1.1,2,3,4,9.0,11,15,1151],[1.1])
+
+
+# TODO: Ezt, hogy lehetne meghívni?
+function (memfun::MemF{RTf,RTc,AT})(argsVect::Vector{Tuple}) where {RTf,RTc,AT}
+    println("MEMO eval for many Tuples")
+    [memfun(args) for args in argsVect]
+    # c3=sort!(ccc)
+    # unique(ccc)
+    # cccunique=Set(ccc)
+end
 
 struct Axis{T} <: AbstractVector{T}
     ticks::Vector{T}
@@ -132,6 +101,20 @@ end
 
 function createAxesGetindexFunction(axes)
      eval(Meta.parse(fncreator(axes)))
+end
+
+function fncreatorTuple(axes)
+    fn="function (ax::$(typeof(axes)))(ind)\n("
+    for i in eachindex(axes)
+        fn*="ax[$i][ind[$i]]"
+        if i < length(axes)
+            fn*=", "
+        end
+    end
+    return fn*")\nend"
+end
+function createAxesGetindexFunctionTuple(axes)
+     eval(Meta.parse(fncreatorTuple(axes)))
 end
 
 
@@ -182,19 +165,28 @@ end
 #     end
 # end
 
-struct NCube{IT<:Integer,FT<:AbstractFloat}
-    corner::Vector{IT} #"bottom-left" #Integer index of the axis
-    size::Vector{IT}#Integer index of the axis
-    posinterp::Vector{FT}#relative coordinate within the cube "(-1:1)" range
+struct NCube{IT<:Integer,FT<:AbstractFloat,ValNdim}
+    corner::MVector{ValNdim,IT} #"bottom-left" #Integer index of the axis
+    size::MVector{ValNdim,IT}#Integer index of the axis
+    posinterp::MVector{ValNdim,FT}#relative coordinate within the cube "(-1:1)" range
     bracketingncube::Bool
-    # gradient ::Vector{T}
+    # gradient ::MVector{MVector{T}}
     # curvnorm::Vector{T}
 end
 
-function allcorner(nc::NCube{IT,FT},T01) where IT where FT
-    [nc.corner .+ nc.size .* T0 for T0 in T01]
+function allcorner(nc::NCube{IT,FT,Ndim},T01)::Vector{MVector} where IT where FT where Ndim
+    [nc.corner .+ nc.size .* T for T in T01]
 end
-/
+
+
+function allcorner(ncubes::Vector{NCube},T01)::Vector{Vector{MVector}}# where IT where FT where Ndim
+        println("asg--------------ggggggggdf")
+    println(length(ncubes))
+    [allcorner(nc,mdbm.T01) for nc in ncubes]
+    # [nc.corner .+ nc.size .* T for nc in ncubes for T in T01]
+end
+
+
 
 
 @generated twopow(::Val{n}) where n = 2^n
@@ -202,6 +194,14 @@ function T01maker(valk::Val{kdim}) where {kdim}
     # T01=[isodd(x÷(2^y)) for y in 0:(kdim-1), x in 0:(2^kdim-1)]
     # SMatrix{kdim,twopow(valk)}(T01)
         SVector{twopow(valk)}([
+        SVector{kdim}([isodd(x÷(2^y)) for y in 0:(kdim-1)])
+         for  x in 0:(2^kdim-1)])
+end
+# //TODO: SH, minek a felső?!?!?!
+function T01maker3(valk::Val{kdim}) where {kdim}
+    # T01=[isodd(x÷(2^y)) for y in 0:(kdim-1), x in 0:(2^kdim-1)]
+    # SMatrix{kdim,twopow(valk)}(T01)
+        SVector{kdim^2}([
         SVector{kdim}([isodd(x÷(2^y)) for y in 0:(kdim-1)])
          for  x in 0:(2^kdim-1)])
 end
@@ -216,11 +216,15 @@ struct MDBM_Problem{N}
     fc::Function
     axes::NTuple{N,Axis}
     ncubes::Vector{NCube}
-    T01::AbstractVector{<:AbstractVector}
+    T01::AbstractVector{<:AbstractVector}#SArray#
     T11pinv::SMatrix
 
-    function MDBM_Problem(fc::Function,axes,ncubes::Vector{NCube{IT,FT}}) where IT where FT
+    function MDBM_Problem(fc::Function,axes,ncubes::Vector{NCube{IT,FT,NdimCube}}) where IT<:Integer where FT<:AbstractFloat where NdimCube
+        # function MDBM_Problem(fc,axes,ncubes)
         Ndim=length(axes)
+        if NdimCube!=Ndim
+            error("axis size is not compatible to the nCube size") # internal check, it should never happend TODO: remove this line
+        end
         # T01=reshape([isodd(x÷(2^y)) for x in 0:(2^Ndim-1) for y in 0:(Ndim-1)],Ndim,(2^Ndim))
         # T11=reshape([isodd(x÷(2^y)) for x in 0:(2^Ndim-1) for y in 0:Ndim],Ndim+1,(2^Ndim))*2.0 .-1.0
         # T11pinv=T11/(2^Ndim)
@@ -228,8 +232,11 @@ struct MDBM_Problem{N}
         T11pinv=T11pinvmaker(Val(Ndim))
         #new(f,c,axes,[NCube(IT.([x...]),ones(IT,length(x)),zeros(FT,length(axes))) for x in Iterators.product((x->1:(length(x.ticks)-1)).(axes)...,)][:])
         createAxesGetindexFunction((axes...,))
+        createAxesGetindexFunctionTuple((axes...,))
+        IT=Int64
+        FT=Float64
         new{Ndim}(fc,(axes...,),
-        [NCube(IT.([x...]),ones(IT,length(x)),Vector{FT}(undef,Ndim),true) for x in Iterators.product((x->1:(length(x.ticks)-1)).(axes)...,)][:]
+        [NCube{IT,FT,Ndim}(SVector{Ndim,IT}([x...]),SVector{Ndim,IT}(ones(IT,length(x))),SVector{Ndim,FT}(zeros(IT,length(x))),true) for x in Iterators.product((x->1:(length(x.ticks)-1)).(axes)...,)][:]
         ,T01,T11pinv)
     end
 end
@@ -237,7 +244,7 @@ end
 
 # function MDBM_Problem{IT,FT}(f::Function, axes::Vector{Axis};constraint::Function=(x...,)->Float16(1.))
 #TODO: ha nincs megadva constraint akkor arra ne csináljon memoization (mert biztosan lassabb lesz!)
-function MDBM_Problem(f::Function, axes::Vector{<:Axis};constraint::Function=(x...,)->true, memoization::Bool=true)#Float16(1.)
+function MDBM_Problem(f::Function, axes::Vector{<:Axis};constraint::Function=(x...,)->true, memoization::Bool=true)#Float16(1.), nothing
     argtypesofmyfunc=map(x->typeof(x).parameters[1], axes);#Argument Type
     AT=Tuple{argtypesofmyfunc...};
     type_f=Base.return_types(f,AT)
@@ -262,43 +269,131 @@ else
     #cons=constraint;
 end
     #TODO: valami ilyesmi a vektorizációhoz
-    #fun=MemF((x)->[f(x...,),constraint(x...,)],Array{MDBMcontainer{RTc,AT}}(undef, 0));
-    MDBM_Problem(fun,axes,Vector{NCube{Int64,Float64}}(undef, 0))
+    Ndim=length(axes)
+    MDBM_Problem(fun,axes,Vector{NCube{Int64,Float64,Ndim}}(undef, 0))
 end
 
-# {AbstractArray{T,1} where T}
-# function MDBM_Problem(f::Function, a::AbstractVector;constraint::Function=(x...,)->Float16(1.))
-function MDBM_Problem(f::Function, a::Vector{<:AbstractVector};constraint::Function=(x...,)->true, memoization::Bool=true)
-    axes=[Axis(ax) for ax in a]
-    argtypesofmyfunc=map(x->typeof(x).parameters[1], axes);#Argument Type
-    AT=Tuple{argtypesofmyfunc...};
-    type_f=Base.return_types(f,AT)
-    if length(type_f)==0
-        error("The input of the function is not compatible with the provided axes")
-    else
-        RTf=type_f[1];#Return Type of f
-    end
+# function MDBM_Problem(f::Function, a::Vector{<:AbstractVector};constraint::Function=(x...,)->true, memoization::Bool=true)
+#     axes=[Axis(ax) for ax in a]
+#     argtypesofmyfunc=map(x->typeof(x).parameters[1], axes);#Argument Type
+#     AT=Tuple{argtypesofmyfunc...};
+#     type_f=Base.return_types(f,AT)
+#     if length(type_f)==0
+#         error("The input of the function is not compatible with the provided axes")
+#     else
+#         RTf=type_f[1];#Return Type of f
+#     end
+#
+#     type_con=Base.return_types(constraint,AT)
+#     if length(type_con)==0
+#         error("The input of the constraint function is not compatible with the provided axes")
+#     else
+#         RTc=type_con[1];#Return Type of the constraint function
+#     end
+#     if memoization
+#         fun=MemF(f,constraint,Array{MDBMcontainer{RTf,RTc,AT}}(undef, 0));
+#         #cons=MemF(constraint,Array{MDBMcontainer{RTc,AT}}(undef, 0));
+#     else
+#         fun=(x)->(f(x...),constraint(x...));
+#         #cons=constraint;
+#     end
+#     Ndim=length(axes)
+#     MDBM_Problem{Ndim}(fun,axes)#,Vector{NCube{Int64,Float64,Val(Ndim)}}(undef, 0))
+# end
 
-    type_con=Base.return_types(constraint,AT)
-    if length(type_con)==0
-        error("The input of the constraint function is not compatible with the provided axes")
-    else
-        RTc=type_con[1];#Return Type of the constraint function
-    end
-    if memoization
-        fun=MemF(f,constraint,Array{MDBMcontainer{RTf,RTc,AT}}(undef, 0));
-        #cons=MemF(constraint,Array{MDBMcontainer{RTc,AT}}(undef, 0));
-    else
-        fun=(x)->(f(x...),constraint(x...));
-        #cons=constraint;
-    end
-    MDBM_Problem(fun,axes,Vector{NCube{Int64,Float64}}(undef, 0))
+
+function allcorner(mdbm::MDBM_Problem{N})::Vector{Vector{SVector}} where N
+    # [allcorner(nc,mdbm.T01) for nc in mdbm.ncubes]
+    [allcorner(nc,mdbm.T01) for nc in mdbm.ncubes]
+    # [nc.corner .+ nc.size .* T for nc in mdbm.ncubes for T in mdbm.T01]
+    # c3=sort!(ccc)
+    # unique(ccc)
+    # cccunique=Set(ccc)
 end
 
-
+# #TODO: vagy kockánként kellene csinálni?
+# function _interpolate!(mdbm,::Type{Val{0}})
+#     issignchangeforeveryfunction=(ncube)->
+#     all(
+#         [
+#             [
+#             any((c)->isless(c[fi],zero(c[fi])),ncube[1])
+#             for fi in 1:length(ncube[1][1])
+#             ]#any positive value
+#
+#             [
+#             any((c)->isless(zero(c[fi]),c[fi]),ncube[1])
+#             for fi in 1:length(ncube[1][1])
+#             ]#anany positive valueny positive value
+#
+#             [
+#             any((c)->isless(zero(c[fi]),c[fi]),ncube[2])
+#             for fi in 1:length(ncube[2][1])
+#             ]#if any positive valueny positive value
+#         ][:]
+#     )#chack for all
+#     isbracketing=[issignchangeforeveryfunction(c) for c in getcornerval(mdbm)]
+#     deleteat!(mdbm.ncubes,.!isbracketing)
+#     ((nc)->nc.posinterp[:].=zero(typeof(nc.posinterp).parameters[1])).(mdbm.ncubes)
+#     return nothing
+# end
+#
+#
+# function _interpolate!(mdbm,::Type{Val{1}})
+#     # Ndim=length(mdbm.axes)
+#     # # TAntansp=A=hcat([[-1,x...] for x in Iterators.product([(-1.0,1.0) for k in 1:k]...)][:]...);
+#     # # #     TAn2=inv(TAn.'*TAn);
+#     # # #     TAtrafo=TAn2*TAn.';
+#     # # TAtrafo = (TAntansp* transpose(TAntansp) ) \ TAntansp
+#     # TAtrafoSHORT=hcat([[-1,x...] for x in Iterators.product([(-1.0,1.0) for k in 1:Ndim]...)][:]...)./(2^Ndim);
+#     #
+#     for nc in mdbm.ncubes
+#         fcvals=getcornerval(nc,mdbm)
+#
+#         if all([
+#             any((c)->isless(zero(c[2][fi]),c[2][fi]),fcvals)
+#             for fi in 1:length(fcvals)
+#             ])# do wh have to compute at all?!?!?! ()
+#             #
+#             TF=typeof(nc).parameters[2]
+#             As = Vector{TF}(undef,0)
+#             ns = Vector{SVector{length(mdbm.axes),TF}}(undef,0)
+#
+#             #for f---------------------
+#             for kf=1:length(fcvals[1][1])
+#                 solloc=mdbm.T11pinv*[fcvals[kcube][1][kf] for kcube=1:length(fcvals)]
+#                 push!(As,solloc[end]);#it is not a real distance within the n-cube (it is ~n*A)!!!
+#                 push!(ns,solloc[1:end-1])
+#             end
+#
+#             #for c---if needed: that is- close to the boundary--------
+#             for kf=1:length(fcvals[1][2])
+#                 if any((c)->isless(c[kf],zero(c[kf])),fcvals[2]) && length(As)<length(mdbm.axes)  # use a constraint till it reduces the dimension to zero (point) and no further
+#                     solloc=mdbm.T11pinv*[fcvals[kcube][2][kf] for kcube=1:length(fcvals)]
+#                     push!(As,solloc[end]);#it is not a real distance within the n-cube (it is ~n*A)!!!
+#                     push!(ns,solloc[1:end-1])
+#                 end
+#             end
+#             nsMAT=hcat(ns...)
+#             #nc.posinterp[:] .= nsMAT * ((transpose(nsMAT) * nsMAT) \ As);
+#             nc.posinterp[:] .= nsMAT * (inv(transpose(nsMAT) * nsMAT) * As);
+#             #for c---------------------
+#         else
+#             nc.posinterp[:] .=1000.0;
+#         end
+#     end
+#
+#     #TODO: what if it falls outside of the n-cube
+#     #TODO: it should be removed ->what shall I do with the bracketing cubes?
+#      #filter!((nc)->norm(nc.posinterp,20.0)>2.0 ,mdbm.ncubes) LinearAlgebre is needed
+#      filter!((nc)->sum(nc.posinterp.^20.0)<(2.0 ^20.0),mdbm.ncubes)#1e6~=(2.0 ^20.0)
+#
+#     return nothing
+# end
 
 #TODO: vagy kockánként kellene csinálni?
 function _interpolate!(mdbm,::Type{Val{0}})
+    (mdbm.fc). ((mdbm.axes).(allcorner(mdbm.ncubes[1],mdbm.T01)))
     issignchangeforeveryfunction=(ncube)->
     all(
         [
@@ -317,7 +412,7 @@ function _interpolate!(mdbm,::Type{Val{0}})
             for fi in 1:length(ncube[2][1])
             ]#if any positive valueny positive value
         ][:]
-    )#chack for all
+    )#check for all
     isbracketing=[issignchangeforeveryfunction(c) for c in getcornerval(mdbm)]
     deleteat!(mdbm.ncubes,.!isbracketing)
     ((nc)->nc.posinterp[:].=zero(typeof(nc.posinterp).parameters[1])).(mdbm.ncubes)
@@ -470,3 +565,62 @@ for i in 1:length(mdbm.axes)]
 #     for nc in mdbm.ncubes]::Vector{typeof(mdbm.axes[i].ticks[1])}
 # for i in eachindex(mdbm.axes)]
 end
+
+
+
+
+
+
+
+
+#-------------------------
+
+# function indexin_sorted(a::Array{T,1}, b::Array{T,1})::Array{Int64,1} where T
+#         # b must contain all the lements of a
+#         # a and b must be sorted
+#     if isempty(a)
+#         return Array{Int64}(undef,0)
+#     elseif length(b) == 1 ##much faster!
+#         return Int64.(a .== b)#*Int64(1)
+#     else
+#         out = Array{Int64}(undef, size(a));#zeros(T, size(a))
+#         leng::Int64 = length(b);
+#
+#         q::Int64 = 1;
+#         q1::Int64 = 1;
+#         q2::Int64 = length(b);
+#         for k = 1:length(a)
+#
+#             if (q2 - q1) == 1
+#                 q = (b[q1] == a[k]) ? q1 : q2
+#             else
+#                 while (b[q] != a[k]) & (q2 > q1 + 1)
+#                     if b[q] > a[k]
+#                         q2 = q
+#                         q =  (q + q1+1) ÷ 2
+#                     else
+#                         q1 = q
+#                         q = (q + q2) ÷ 2
+#                     end
+#             #print([q1;q;q2])
+#             #print([b[q]])
+#             #println([a[k]])
+#                 end
+#             end
+#             out[k] = b[q] == a[k] ? q : 0
+#             q = max(out[k], q1)
+#             q1 = q
+#             q2 = length(b)
+#           #print("-----")
+#           #print(out[1:k])
+#           #println("----")
+#             if q1 > length(b)#all the element is larger than the last one
+#                 break
+#             end
+#         end
+#         return out
+#     end
+# end
+#
+#
+# # indexin_sorted([-5,0,1,1.1,2,3,4,9.0,11,15,1151],[1.1])
--- src/test_mdbmProblem.jl
+++ src/test_mdbmProblem.jl
@@ -84,7 +84,7 @@ scatter(x,y)
 
 
 
-# szemét--------------------------------
+#--------------- szemét--------------------------------
 # @time mdbm.fc(1.0,2.0,2.0)
 # @time mdbm.fc(1.0,2.0,rand(Float64,1)...)
 # @time mdbm.fc(1.0,2.0,3.0)
@@ -97,20 +97,69 @@ scatter(x,y)
 println("<<<<<<<<<<<<<<<<<<")
 nc=mdbm.ncubes[1]
 
-
-
+typeof(allcorner(nc,mdbm.T01))
 @code_warntype allcorner(nc,mdbm.T01)
+@code_warntype allcorner(mdbm.ncubes,mdbm.T01)
+
+
+
+@code_warntype allcorner(mdbm)
+@code_warntype ((allcorner(mdbm.ncubes,mdbm.T01)))
+@code_warntype (mdbm.axes.(allcorner(nc,mdbm.T01)))
+@code_warntype map((mdbm.axes),Base.Iterators.flatten(allcorner(mdbm)))
+typeof(map((mdbm.axes),Base.Iterators.flatten(allcorner(mdbm))))
+(mdbm.fc).(map((mdbm.axes),Base.Iterators.flatten(allcorner(mdbm))))
+map((mdbm.fc) ∘ (mdbm.axes),Base.Iterators.flatten(allcorner(mdbm)))
+@code_warntype (mdbm.fc).(map((mdbm.axes),Base.Iterators.flatten(allcorner(mdbm))))
+@code_warntype map((mdbm.fc) ∘ (mdbm.axes),Base.Iterators.flatten(allcorner(mdbm)))
+@time (mdbm.fc).(map((mdbm.axes),Base.Iterators.flatten(allcorner(mdbm))))
+@time map((mdbm.fc) ∘ (mdbm.axes),Base.Iterators.flatten(allcorner(mdbm)))
+@time map((mdbm.fc) ∘ (mdbm.axes).(allcorner(mdbm)))
+(mdbm.fc). ((mdbm.axes).(allcorner(mdbm.ncubes[1],mdbm.T01)))
+
+@time
+
+mdbm.fc(111.7,Float16(1.2),31.3)
+[mdbm.fc.memoryacc[1],length(mdbm.fc.fvalarg)]
+
+mdbm.fc
+mdbm.fc((2.0,Float16(1.6),3.3))
+mdbm.fc([(2.0,Float16(1.6),3.3),(5.0,Float16(1.6),3.3),(2.0,Float16(1.6),3.3)])
+
+
+
+location=searchsortedfirst(mdbm.fc.fvalarg,(1.7, 1.2, 3.3),lt=(x,y)->isless(x.callargs,y));
+
+
+
+
+
+
+
+collect(Base.Iterators.flatten(allcorner(mdbm)))
+@code_warntype (x->mdbm.fc(x...)).(map((mdbm.axes),Base.Iterators.flatten(allcorner(mdbm))))
+
+cc=allcorner(nc,mdbm.T01)[1]
+mdbm.axes.(cc)
+typeof(mdbm.ncubes[1].corner)
+
+mdbm.T01[1]
 @code_warntype mdbm.axes(3,4,2)
-println(".................----------------------<<<<<<<<<<<<<<<<<<"),
+println(".................----------------------<<<<<<<<<<<<<<<<<<")
+
+
+
 
 map(mdbm.axes,[1,1,3])
 @code_warntype getcornerval(mdbm)
 @time _interpolate!(mdbm,Val{1})
-
+typeof(mdbm.ncubes[1].corner[1])
 # @code_llvm getcornerval(mdbm)
 
 nc.corner::Vector{IT}.+((mdbm.T01).*(nc.size::Vector{IT}))
-mdbm.axes
+@code_warntype mdbm.axes(1,2,3)
+@code_warntype mdbm.axes([1,2,3])
+@code_warntype mdbm.axes((1,2,3))
 println(".................----------------------<<<<<<<<<<<<<<<<<<")
 @code_warntype mdbm.c(1.2,3.2,7.2)
 
@@ -128,7 +177,9 @@ Ndim=3
 T01=[[isodd(x÷(2^y)) for y in 0:(Ndim-1)] for x in 0:(2^Ndim-1) ]
 
 Base.:*.(T01,Ref(nc.size))
+typeof(nc.corner)
 
+Vector{Float32}(2)
 T01[:].*nc.size
 
 nc.corner::Vector{IT}.+(((nc.size::Vector{IT}*).()
