1bf611e7bf70448275a53ec8c26b6948b9657b45
BachrathyD <bachrathy@mm.bme.hu>
2025-04-30 11:52:42 +0200

    bug fix - generate vector of simplexs

--- temp_nc_subinterp_test_v2.jl
+++ temp_nc_subinterp_test_v2.jl
@@ -25,30 +25,27 @@ for nc in mymdbm.ncubes
     fixed_dims = Vector{Tuple{Int,Bool}}(undef, 0)
     nc_template = mymdbm.T01
 
+    
+    posall_tree = MDBM.PositionTree(nc.posinterp)
+    edges, all_edge_fixed_dims = MDBM.generate_sub_faces(nc_template, fixed_dims)
 
-    posall_tree = MDBM.PositionTree(NaN * Vector{Float64}(undef, N))
-    MDBM.interpsubcubesolution!(posall_tree, [nc_template], [fixed_dims], nccorner, ncsize, mymdbm)
-    #    
-    #    @show posall_tree
+    MDBM.interpsubcubesolution!(posall_tree, edges, all_edge_fixed_dims, nccorner, ncsize, mymdbm)
+
     path2points = MDBM.extract_paths(posall_tree)
 
 
 
 
+    puniq = unique(collect(Iterators.flatten(path2points)))
 
+    puniq_pos=getinterpolatedsolution.(puniq, Ref(nc.corner), Ref(mymdbm.axes))
+    path2points_pos = [getinterpolatedsolution.(inner, Ref(nc.corner), Ref(mymdbm.axes)) for inner in path2points]
 
-
-    #f = Figure()
-    puniq = unique(collect(Iterators.flatten(path2points)))[2:end]
-
-
-    #TODO ezt dmineziótlan ben kell csinálni!!!!!!!!!!!!!!!!!!!!!!!!
-
-    xyz_sol = getinterpolatedsolution(nc, mymdbm)
-    gxyz = getinterpolatedgradient(nc, mymdbm)
+    xyz_sol = nc.posinterp
+    gxyz = nc.gradient
 
     total = 0.0
-    @inbounds for g in gxyz
+    @inbounds for g in eachcol(gxyz)
         for p in puniq
             total += abs(dot(p .- xyz_sol, g) / norm(g))
         end
@@ -57,9 +54,9 @@ for nc in mymdbm.ncubes
     #    arrowcolor = strength, linecolor = strength)
 
   #  if N == 3
-  #      scatter!(getindex.(puniq, 1), getindex.(puniq, 2), getindex.(puniq, 3))
+  #      scatter!(getindex.(puniq_pos, 1), getindex.(puniq_pos, 2), getindex.(puniq_pos, 3))
   #  else
-  #      scatter!(getindex.(puniq, 1), getindex.(puniq, 2))
+  #      scatter!(getindex.(puniq_pos, 1), getindex.(puniq_pos, 2))
   #  end
 
 
@@ -71,14 +68,18 @@ for nc in mymdbm.ncubes
 
     pathall_ncube = Vector{SVector{N,T}}()
     #pathloc=path2points[1]
-    for pathloc in path2points
+    for pathloc in path2points_pos
         if length(pathloc) > 2
+
+            push!(pathall, fill(NaN, N))
             append!(pathall, pathloc)
+            push!(error_all, NaN)
             append!(error_all, ones(length(pathloc)) .* total)
             #append!(pathall_ncube, vcat(pathloc, [pathloc[2]])) #closed traingle for line plotting
         end
     end
     #lines!(getindex.(pathall,1),getindex.(pathall,2),color=getindex.(pathall,1) .* 0 .+ total)
+    #lines!(getindex.(pathall,1),getindex.(pathall,2),getindex.(pathall,3),color=getindex.(pathall,1) .* 0 .+ total)
 
     # #plot each ncubes separately
     # if length(pathall_ncube) > 1
@@ -88,9 +89,11 @@ for nc in mymdbm.ncubes
 end
 
 end
+
+
 f = Figure()
-faces = (1:4:(length(pathall))) .+ collect(1:N)'
-mesh(pathall, faces, alpha=1.0)
+n_faces = (1:4:(length(pathall))) .+ collect(1:N)'
+mesh(pathall, n_faces, alpha=1.0)
 
 
 if N == 3
