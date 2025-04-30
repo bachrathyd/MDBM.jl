d447599a8b4baac4a488ac929b6ffdaef99e1ec6
BachrathyDaniel <bachrathyd@gmail.com>
2025-04-30 08:28:20 +0200

    w.i.p.

--- temp_nc_subinterp_test_v2.jl
+++ temp_nc_subinterp_test_v2.jl
@@ -14,76 +14,77 @@ pathall = Vector{SVector{N,T}}()
 error_all = Vector{Float64}()
 
 for nc in mymdbm.ncubes
-# k = 0
-# k = k + 1
-# nc = mymdbm.ncubes[k]
+    # k = 0
+    # k = k + 1
+    # nc = mymdbm.ncubes[k]
 
 
-nccorner = nc.corner
-ncsize = nc.size
+    nccorner = nc.corner
+    ncsize = nc.size
 
-fixed_dims = Vector{Tuple{Int,Bool}}(undef, 0)
-nc_template = mymdbm.T01
-
-
-posall_tree = MDBM.PositionTree(NaN * Vector{Float64}(undef, N))
-MDBM.interpsubcubesolution!(posall_tree, [nc_template], [fixed_dims], nccorner, ncsize, mymdbm)
-#    
-#    @show posall_tree
-path2points = MDBM.extract_paths(posall_tree)
+    fixed_dims = Vector{Tuple{Int,Bool}}(undef, 0)
+    nc_template = mymdbm.T01
 
 
-
+    posall_tree = MDBM.PositionTree(NaN * Vector{Float64}(undef, N))
+    MDBM.interpsubcubesolution!(posall_tree, [nc_template], [fixed_dims], nccorner, ncsize, mymdbm)
+    #    
+    #    @show posall_tree
+    path2points = MDBM.extract_paths(posall_tree)
 
 
 
 
-#f = Figure()
-puniq = unique(collect(Iterators.flatten(path2points)))[2:end]
 
 
-#TODO ezt dmineziótlan ben kell csinálni!!!!!!!!!!!!!!!!!!!!!!!!
 
-xyz_sol = getinterpolatedsolution(nc, mymdbm)
-gxyz = getinterpolatedgradient(nc, mymdbm)
+    #f = Figure()
+    puniq = unique(collect(Iterators.flatten(path2points)))[2:end]
 
-total = 0.0
-@inbounds for g in gxyz
-    for p in puniq
-        total += abs(dot(p .- xyz_sol, g))
+
+    #TODO ezt dmineziótlan ben kell csinálni!!!!!!!!!!!!!!!!!!!!!!!!
+
+    xyz_sol = getinterpolatedsolution(nc, mymdbm)
+    gxyz = getinterpolatedgradient(nc, mymdbm)
+
+    total = 0.0
+    @inbounds for g in gxyz
+        for p in puniq
+            total += abs(dot(p .- xyz_sol, g) / norm(g))
+        end
     end
-end
 
-#    arrowcolor = strength, linecolor = strength)
+    #    arrowcolor = strength, linecolor = strength)
 
-
-#scatter!(getindex.(puniq,1),getindex.(puniq,2),getindex.(puniq,3))
-#scatter!(getindex.(puniq, 1), getindex.(puniq, 2))
-
-
-
-#plot each line element separately
-#[plot!(collect(eachrow(reduce(hcat, pathloc)))...,lw=5) for pathloc in path2points]
-
-   # pathall = Vector{SVector{N,T}}()
-
-
-pathall_ncube = Vector{SVector{N,T}}()
-#pathloc=path2points[1]
-for pathloc in path2points
-    if length(pathloc) > 2
-        append!(pathall, pathloc)
-        append!(error_all,ones(length(pathloc)) .* total)
-        #append!(pathall_ncube, vcat(pathloc, [pathloc[2]])) #closed traingle for line plotting
+    if N == 3
+        scatter!(getindex.(puniq, 1), getindex.(puniq, 2), getindex.(puniq, 3))
+    else
+        scatter!(getindex.(puniq, 1), getindex.(puniq, 2))
     end
-end
-#lines!(getindex.(pathall,1),getindex.(pathall,2),color=getindex.(pathall,1) .* 0 .+ total)
 
-# #plot each ncubes separately
-# if length(pathall_ncube) > 1
-#     plot!(collect(eachrow(reduce(hcat, pathall_ncube)))[1:3]..., lw=5)
-# end
-#append!(pathall, pathall_ncube)
+
+    #plot each line element separately
+    #[plot!(collect(eachrow(reduce(hcat, pathloc)))...,lw=5) for pathloc in path2points]
+
+    # pathall = Vector{SVector{N,T}}()
+
+
+    pathall_ncube = Vector{SVector{N,T}}()
+    #pathloc=path2points[1]
+    for pathloc in path2points
+        if length(pathloc) > 2
+            append!(pathall, pathloc)
+            append!(error_all, ones(length(pathloc)) .* total)
+            #append!(pathall_ncube, vcat(pathloc, [pathloc[2]])) #closed traingle for line plotting
+        end
+    end
+    #lines!(getindex.(pathall,1),getindex.(pathall,2),color=getindex.(pathall,1) .* 0 .+ total)
+
+    # #plot each ncubes separately
+    # if length(pathall_ncube) > 1
+    #     plot!(collect(eachrow(reduce(hcat, pathall_ncube)))[1:3]..., lw=5)
+    # end
+    #append!(pathall, pathall_ncube)
 end
 
 
@@ -91,9 +92,15 @@ end
 #mesh!(pathall, faces, alpha=1.0)
 
 
-lines!(getindex.(pathall,1),getindex.(pathall,2),color=error_all)
+if N == 3
+    lines!(getindex.(pathall, 1), getindex.(pathall, 2), getindex.(pathall, 3), color=log.(error_all), linewidth=3)
+else
+    lines!(getindex.(pathall, 1), getindex.(pathall, 2), color=log.(error_all), linewidth=3)
+end
 #plot(getindex.(pathall,1),getindex.(pathall,2),getindex.(pathall,3),lw=5)
 
 
-Colorbar(f[1, 2], limits = (minimum(error_all), maximum(error_all)), colormap = :viridis,
-    flipaxis = false)
+Colorbar(f[1, 2], limits=(log(minimum(error_all)), log(maximum(error_all))), colormap=:viridis,
+    flipaxis=false)
+
+f
--- temp_test_connect_.jl
+++ temp_test_connect_.jl
@@ -4,57 +4,57 @@ using MDBM
 
 using GLMakie
 
-#using Plots
-#theme(:dark)#:vibrant:dracula:rose_pine
-#plotly()
-##const PLOTS_DEFAULTS = Dict(:theme => :wong, :fontfamily => "Computer Modern", :label => nothing, :dpi => 600 )const PLOTS_DEFAULTS = Dict(:theme => :wong, :fontfamily => "Computer Modern", :label => nothing, :dpi => 600 )
-#default(size=(1500, 1200), titlefont=(15, "times"), legendfontsize=13, guidefont=(12, :white), tickfont=(12, :orange), guide="x", framestyle=:zerolines, yminorgrid=true, fontfamily="Computer Modern", label=nothing, dpi=600)
-
-f = Figure()
-
-#Solution of an uncertain implicit equation
-
-function foo4(x, y, z, r)
-    x^2.0 + y^2.0 + z - r^2.0, z - y#,x-sin(z)#
-end
-function foo3(x, y, z)
-    x^2.0 + y^2.0 + z - 2.0^2.0, x - sin(z)#,z-y#
-end
-function foo2(x, y)
-    x^2.0 + y^2.0 - 2.0^2.0
-end
-
-mymdbm = MDBM_Problem(foo3, [-3.0:3.0, -3.0:3.0, -3.0:3.0])
-# mymdbm=MDBM_Problem(foo2,[-3.0:3.0,-3.0:3.0])
-solve!(mymdbm, 1)
-
-
-
-
-#interpolate!(mymdbm,interpolationorder=0)
-interpolate!(mymdbm, interpolationorder=1)
-
-xyz_sol = getinterpolatedsolution(mymdbm)
-scatter(xyz_sol...)
-
-#plot!(xticks =mymdbm.axes[1].ticks,yticks =mymdbm.axes[2].ticks)
-
-
-DT1 = connect(mymdbm)
-edge2plot_xyz = [reduce(hcat, [i_sol[getindex.(DT1, 1)], i_sol[getindex.(DT1, 2)], fill(NaN, length(DT1))])'[:] for i_sol in xyz_sol]
-
-lines!(edge2plot_xyz...)
-#for edges in DT1
-#plot!(
-#    x_sol[[edges...]],
-#    y_sol[[edges...]],
-#    z_sol[[edges...]])
-#end
-
-
-
-
-
+# # #using Plots
+# # #theme(:dark)#:vibrant:dracula:rose_pine
+# # #plotly()
+# # ##const PLOTS_DEFAULTS = Dict(:theme => :wong, :fontfamily => "Computer Modern", :label => nothing, :dpi => 600 )const PLOTS_DEFAULTS = Dict(:theme => :wong, :fontfamily => "Computer Modern", :label => nothing, :dpi => 600 )
+# # #default(size=(1500, 1200), titlefont=(15, "times"), legendfontsize=13, guidefont=(12, :white), tickfont=(12, :orange), guide="x", framestyle=:zerolines, yminorgrid=true, fontfamily="Computer Modern", label=nothing, dpi=600)
+# # 
+# # f = Figure()
+# # 
+# # #Solution of an uncertain implicit equation
+# # 
+# # function foo4(x, y, z, r)
+# #     x^2.0 + y^2.0 + z - r^2.0, z - y#,x-sin(z)#
+# # end
+# # function foo3(x, y, z)
+# #     x^2.0 + y^2.0 + z - 2.0^2.0, x - sin(z)#,z-y#
+# # end
+# # function foo2(x, y)
+# #     x^2.0 + y^2.0 - 2.0^2.0
+# # end
+# # 
+# # mymdbm = MDBM_Problem(foo3, [-3.0:3.0, -3.0:3.0, -3.0:3.0])
+# # # mymdbm=MDBM_Problem(foo2,[-3.0:3.0,-3.0:3.0])
+# # solve!(mymdbm, 1)
+# # 
+# # 
+# # 
+# # 
+# # #interpolate!(mymdbm,interpolationorder=0)
+# # interpolate!(mymdbm, interpolationorder=1)
+# # 
+# # xyz_sol = getinterpolatedsolution(mymdbm)
+# # scatter(xyz_sol...)
+# # 
+# # #plot!(xticks =mymdbm.axes[1].ticks,yticks =mymdbm.axes[2].ticks)
+# # 
+# # 
+# # DT1 = connect(mymdbm)
+# # edge2plot_xyz = [reduce(hcat, [i_sol[getindex.(DT1, 1)], i_sol[getindex.(DT1, 2)], fill(NaN, length(DT1))])'[:] for i_sol in xyz_sol]
+# # 
+# # lines!(edge2plot_xyz...)
+# # #for edges in DT1
+# # #plot!(
+# # #    x_sol[[edges...]],
+# # #    y_sol[[edges...]],
+# # #    z_sol[[edges...]])
+# # #end
+# # 
+# # 
+# # 
+# # 
+# # 
 
 
 
@@ -62,9 +62,6 @@ lines!(edge2plot_xyz...)
 
 
 #-----------------------------
-f = Figure()
-
-GLMakie.Axis(f[1, 1])
 
 
 function foo2(x, y)
@@ -73,17 +70,26 @@ function foo2(x, y)
         println("x,y=$x, $y")
         return 0001.0001
     else
-        #(x^-1.0 + y^-1.0 - 2.0^2.0)^1.0
-        -(-x^2.0 + y^1.0 - 0.0*2.0^2.0)^1.0
+        (x^-1.0 + y^-1.0 - 2.0^2.0)^1.0
+        #-(-x^2.0 + y^1.0 - 0.0*2.0^2.0)^1.0
         #(x^2.0 + y^2.0 - 2.0^2.0)^1.0
+        #(x^4.0 + y^3.0 - 2.0^2.0)^1.0
     end
 end
-mymdbm = MDBM_Problem(foo2, [[0.101021010, 3.0], 0.0015205: 3.0])
-solve!(mymdbm, 5)
+mymdbm = MDBM_Problem(foo2, [[-1.101021010, 3.02751], [-1.0015205, 3.02574]])
+#mymdbm = MDBM_Problem(foo2, [-3.0:3.0, -3.0:3.0])
+#mymdbm = MDBM_Problem(foo2, [-3.0:3.0, -3.0:3.0])
+solve!(mymdbm, 2)
 
 #mymdbm=MDBM_Problem(foo2,[-5:5,-3:3])
 #solve!(mymdbm,2)
 
+
+f = Figure()
+
+kwargs = (; xminorticksvisible = true, xminorgridvisible = true, yminorticksvisible = true, yminorgridvisible = true)
+GLMakie.Axis(f[1, 1]; xminorticks = mymdbm.axes[1].ticks, yminorticks  = mymdbm.axes[2].ticks, kwargs...)
+#), yminorticks = mymdbm.axes[2].ticks,
 
 #interpolate!(mymdbm,interpolationorder=0)
 interpolate!(mymdbm, interpolationorder=1)
@@ -105,7 +111,7 @@ xyz_sol = getinterpolatedsolution(mymdbm)
 gxyz=getinterpolatedgradient(mymdbm.ncubes,mymdbm)
 
 
-arrows!(xyz_sol..., gxyz[1]..., arrowsize = 10, lengthscale = 0.3)#    arrowcolor = strength, linecolor = strength)
+#arrows!(xyz_sol..., gxyz[1]..., arrowsize = 10, lengthscale = 0.3)#    arrowcolor = strength, linecolor = strength)
 
 
 
@@ -118,13 +124,13 @@ arrows!(xyz_sol..., gxyz[1]..., arrowsize = 10, lengthscale = 0.3)#    arrowcolo
 
 
 
- fi = LinRange(0, 2pi, 5000)
- lines!(2 .* sin.(fi), 2 .* cos.(fi))
-# #lines!(xticks =mymdbm.axes[1].ticks,yticks =mymdbm.axes[2].ticks)
+# fi = LinRange(0, 2pi, 5000)
+# lines!(2 .* sin.(fi), 2 .* cos.(fi))
+# # #lines!(xticks =mymdbm.axes[1].ticks,yticks =mymdbm.axes[2].ticks)
+# 
+# 
 
-
-DT1 = connect(mymdbm)
-edge2plot_xyz = [reduce(hcat, [i_sol[getindex.(DT1, 1)], i_sol[getindex.(DT1, 2)], fill(NaN, length(DT1))])'[:] for i_sol in xyz_sol]
-
-lines!(edge2plot_xyz...)
-#for edges in DT1
+# DT1 = connect(mymdbm)
+# edge2plot_xyz = [reduce(hcat, [i_sol[getindex.(DT1, 1)], i_sol[getindex.(DT1, 2)], fill(NaN, length(DT1))])'[:] for i_sol in xyz_sol]
+#  
+# lines!(edge2plot_xyz...)
